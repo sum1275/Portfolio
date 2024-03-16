d7e047c19d79604a03d5f76d4ae9c2304b2da544
Sumit Sinha <sumitsinha215@gmail.com>
2024-03-16 09:35:43 +0530

    changed the title

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -42,7 +42,7 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
+  title: 'Sumit',
   description: "Example site built with Tim Baker's react resume template",
 };
 
